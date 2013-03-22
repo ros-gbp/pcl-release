156e23e0406a22aef98a1f944b201a4c40aa1938
Julius Kammerl <julius@kammerl.de>
2013-03-22 01:20:36 -0700

    patches for upstream from groovy-patches

--- common/include/pcl/point_types.h
+++ common/include/pcl/point_types.h
@@ -53,7 +53,10 @@
 
 // We're doing a lot of black magic with Boost here, so disable warnings in Maintainer mode, as we will never
 // be able to fix them anyway
-#pragma warning(disable: 4201)
+#if defined _MSC_VER
+  #pragma warning(disable: 4201)
+#endif
+
 //#pragma warning(push, 1)
 #ifdef BUILD_Maintainer
 #  if defined __GNUC__
@@ -185,8 +188,7 @@ namespace pcl
     * \ingroup common
     * \deprecated USE SHOT352 FOR SHAPE AND SHOT1344 FOR SHAPE+COLOR INSTEAD
     */
-  struct
-  PCL_DEPRECATED_CLASS (SHOT, "USE SHOT352 FOR SHAPE AND SHOT1344 FOR SHAPE+COLOR INSTEAD");
+  struct SHOT;
 
   /** \brief Members: float descriptor[352], rf[9]
     * \ingroup common
@@ -587,7 +589,9 @@ namespace pcl {
   };
 } // namespace pcl
 
-#pragma warning(default: 4201)
+#if defined _MSC_VER
+  #pragma warning(default: 4201)
+#endif
 //#pragma warning(pop)
 #ifdef BUILD_Maintainer
 #  if defined __GNUC__
